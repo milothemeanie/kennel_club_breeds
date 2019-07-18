4908befa98a85fc6788019371913e343af5652ce
cward <milothemeanie@gmail.com>
2019-07-17 22:32:52 -0500

    updated to current elements

--- KennelScrape.py
+++ KennelScrape.py
@@ -13,12 +13,12 @@ def main():
         for breed in breed_list:
             print(breed)
 
-        first_breed = breed_list[0]
-        page = requests.get(URL.format(first_breed))
+        first_breed = breed_list[1]
+        page = requests.get(first_breed)
 
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'html.parser')
-            print(soup.find("div", {"breed-details__main"}).find_all("li"))
+            print(soup.find("ul", {"class": "attribute-list"}).find_all("li"))
 
     except Exception as ex:
         print("Failed kennel scrape " + repr(ex))
@@ -28,7 +28,7 @@ def retrieve_breed_list():
     page = requests.get(URL.format(ALL_BREEDS))
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
-        breeds = soup.find("select", {"name": "allbreedsbyname"}) \
+        breeds = soup.find("select", {"id": "breed-search"}) \
             .find_all("option", {"value": True})
 
         return [x['value'] for x in breeds]
