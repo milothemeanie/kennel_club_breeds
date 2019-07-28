b0fc7c640dceba2161c5e92eda1d53b9a4d4ce84
cward <milothemeanie@gmail.com>
2019-07-28 12:42:26 -0500

    ability to save raw breed info to database

---
+++ CreateDatabase.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect('/home/cward/PycharmProjects/kennel_club_breeds/kennel.db')
+
+create = "CREATE TABLE breed_raw (" \
+         "id INTEGER PRIMARY KEY AUTOINCREMENT ," \
+         "breed TEXT NOT NULL," \
+         "temperament_raw TEXT NULL," \
+         "popularity_raw TEXT NULL, " \
+         "height_raw TEXT NULL," \
+         "weight_raw TEXT NULL," \
+         "expectancy_raw TEXT NULL," \
+         "group_raw TEXT NULL)"
+
+conn.execute(create)
+
+# class Info(Enum):
+#     temperament = 0
+#     popularity = 1
+#     height = 2
+#     weight = 3
+#     expectancy = 4
+#     group = 5
+
+
+# Temperament: Playful, Perky, Smart
+# Popularity: Ranks 122 of 193
+# Height: 9-12 inches (toy), 12-15 inches (miniature), 15-19 inches (Standard)
+# Weight: 6-10 pounds (toy), 10-20 pounds (miniature), 25-35 pounds (standard)
+# Life Expectancy: 13-15 years
+# Group: Non-Sporting Group
--- KennelScrape.py
+++ KennelScrape.py
@@ -1,3 +1,7 @@
+import sqlite3
+from collections import defaultdict
+from enum import Enum
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,6 +9,16 @@ URL = "http://www.akc.org/dog-breeds/{}"
 
 ALL_BREEDS = "all-breeds"
 
+DB = '/home/cward/PycharmProjects/kennel_club_breeds/kennel.db'
+
+
+# Example breed description
+# Temperament: Playful, Perky, Smart
+# Popularity: Ranks 122 of 193
+# Height: 9-12 inches (toy), 12-15 inches (miniature), 15-19 inches (Standard)
+# Weight: 6-10 pounds (toy), 10-20 pounds (miniature), 25-35 pounds (standard)
+# Life Expectancy: 13-15 years
+# Group: Non-Sporting Group
 
 def main():
     try:
@@ -13,15 +27,17 @@ def main():
         for breed_link in breed_list[1:]:
             # first_breed = breed_list[1]
             page = requests.get(breed_link)
-            desc = dict()
+            desc = defaultdict(lambda: None)
             desc["breed"] = breed_link.split("/")[-2]
 
             if page.status_code == 200:
                 soup = BeautifulSoup(page.content, 'html.parser')
                 lis = soup.find("ul", {"class": "attribute-list"}).find_all("li")
-                for li in lis:
+                for i, li in enumerate(lis):
                     span = li.find_all("span", {"class", "attribute-list__description"})
-                    print(span[0].text)
+                    value = span[0].text
+                    desc[determine_attr(i, value).name] = value
+                insert_breed_raw(desc)
 
     except Exception as ex:
         print("Failed kennel scrape " + repr(ex))
@@ -38,6 +54,42 @@ def retrieve_breed_list():
 
     else:
         raise Exception('Failed the request for kennel breed list, request code' + page.status_code)
+
+
+def determine_attr(i, text):
+    if i == 0:
+        return Info.temperament
+    elif text.startswith("Rank"):
+        return Info.popularity
+    elif "inches" in text:
+        return Info.height
+    elif "pounds" in text:
+        return Info.weight
+    elif text.endswith("years"):
+        return Info.expectancy
+    else:
+        return Info.group
+
+
+def insert_breed_raw(desc):
+    print("inserting ", desc)
+    conn = sqlite3.connect(DB)
+    try:
+        conn.execute("INSERT INTO breed_raw "
+                     "  (breed, temperament_raw, popularity_raw, height_raw ,weight_raw,expectancy_raw, group_raw)"
+                     "VALUES (:breed, :temperament, :popularity, :height, :weight, :expectancy, :group)", desc)
+        conn.commit()
+    finally:
+        conn.close()
+
+
+class Info(Enum):
+    temperament = 0
+    popularity = 1
+    height = 2
+    weight = 3
+    expectancy = 4
+    group = 5
 
 
 if __name__ == '__main__':
